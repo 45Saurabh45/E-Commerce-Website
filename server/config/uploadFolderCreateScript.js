027f757ede824d77ce38d728f8e7f995171c8149
saurabh <10c.saurabhtiwari123@gmail.com>
2024-07-28 23:01:05 +0530

    final changes

--- server/config/uploadFolderCreateScript.js
+++ server/config/uploadFolderCreateScript.js
@@ -1,63 +1,27 @@
-const express = require("express");
-const path = require("path"); // Import path module for handling file paths
-const app = express();
-require("dotenv").config();
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const fs = require("fs");
 
-// Import Router
-const authRouter = require("./routes/auth");
-const categoryRouter = require("./routes/categories");
-const productRouter = require("./routes/products");
-const brainTreeRouter = require("./routes/braintree");
-const orderRouter = require("./routes/orders");
-const usersRouter = require("./routes/users");
-const customizeRouter = require("./routes/customize");
-// Import Auth middleware for check user login or not~
-const { loginCheck } = require("./middleware/auth");
-const CreateAllFolder = require("./config/uploadFolderCreateScript");
+const categoriesFolder = "./public/uploads/categories";
+const customizeFolder = "./public/uploads/customize";
+const productsFolder = "./public/uploads/products";
 
-CreateAllFolder();
+const CreateAllFolder = () => {
+  if (!fs.existsSync(categoriesFolder)) {
+    fs.mkdirSync(categoriesFolder, {
+      recursive: true,
+    });
+  }
 
-mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    console.log(
-      "==============Mongodb Database Connected Successfully=============="
-    )
-  )
-  .catch((err) => console.log(err));
+  if (!fs.existsSync(customizeFolder)) {
+    fs.mkdirSync(customizeFolder, {
+      recursive: true,
+    });
+  }
 
-// Middleware
-app.use(morgan("dev"));
-app.use(cookieParser());
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  if (!fs.existsSync(productsFolder)) {
+    fs.mkdirSync(productsFolder, {
+      recursive: true,
+    });
+  }
+};
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// API routes
-app.use("/api", authRouter);
-app.use("/api/user", usersRouter);
-app.use("/api/category", categoryRouter);
-app.use("/api/product", productRouter);
-app.use("/api", brainTreeRouter);
-app.use("/api/order", orderRouter);
-app.use("/api/customize", customizeRouter);
-
-// Serve React app for any other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = CreateAllFolder;
